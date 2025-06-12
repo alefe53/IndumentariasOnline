811a05ac203bfe84c9b42b04f475f86b7fec3966
Juan Forcada <juanforcada0@gmail.com>
2025-06-12 19:36:37 -0300

    Agregue los views, y al app.vue

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 // src/app.js
 
 import express from "express";
+import path from "path";
 import { config } from "../config/config.js";
 import homeRouter from "./routers/home.router.js";
 
 const app = express();
+
+// Configuración de vistas
+app.set("views", path.join(process.cwd(), "src", "views"));
+app.set("view engine", "ejs");
 
 // Especificar la ubicación de tus archivos de plantillas (views)
 app.set("views", config.paths.VIEWS);
@@ -16,11 +21,11 @@ app.use("/home", homeRouter);
 
 // --- RUTAS GENERALES DE LA APLICACIÓN ---
 app.get("/", (req, res) => {
-	//res.json({url:config.SUPABASE_URL}) //prueba de que me conecto a
-	res.redirect("/home");
+  //res.json({url:config.SUPABASE_URL}) //prueba de que me conecto a
+  res.redirect("/home");
 });
 
 app.listen(config.server.PORT, () => {
-	const message = `👓 🤖 SERVER is UP at http://${config.server.HOST}:${config.server.PORT} 🤖`;
-	console.log(message);
+  const message = `👓 🤖 SERVER is UP at http://${config.server.HOST}:${config.server.PORT} 🤖`;
+  console.log(message);
 });
--- src/controllers/homepage.controller.js
+++ src/controllers/homepage.controller.js
@@ -3,26 +3,24 @@
 import { getProductosDestacados } from "../services/producto.service.js";
 
 export const getHomePage = async (req, res, next) => {
-	try {
-		console.log("Controlador: Solicitando página de inicio...");
+  try {
+    console.log("Controlador: Solicitando página de inicio...");
 
-		const productos = await getProductosDestacados();
-		console.log(
-			"Controlador: Productos recibidos del servicio:",
-			productos.length > 0
-				? `${productos.length} productos`
-				: "Ningún producto",
-		);
+    const productos = await getProductosDestacados();
+    console.log(
+      "Controlador: Productos recibidos del servicio:",
+      productos.length > 0 ? `${productos.length} productos` : "Ningún producto"
+    );
 
-		res.render("homepage.ejs", {
-			pageTitle: "IndumentariasOnline - Inicio",
-			productos: productos,
-		});
-	} catch (error) {
-		console.error(
-			"Error en homepage.controller al intentar obtener productos y renderizar la página de inicio",
-			error,
-		);
-		next(error);
-	}
+    res.render("home", {
+      pageTitle: "IndumentariasOnline - Inicio",
+      productos: productos,
+    });
+  } catch (error) {
+    console.error(
+      "Error en homepage.controller al intentar obtener productos y renderizar la página de inicio",
+      error
+    );
+    next(error);
+  }
 };

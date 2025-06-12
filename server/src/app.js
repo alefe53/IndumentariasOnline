ec2734b2d3d9ab3aac75153e60942a8d0d457c1d
Juan Forcada <juanforcada0@gmail.com>
2025-06-12 20:17:34 -0300

    Actualizacion de las rutas

--- server/src/app.js
+++ server/src/app.js
@@ -8,20 +8,17 @@ import homeRouter from "./routers/home.router.js";
 const app = express();
 
 // Configuración de vistas
-app.set("views", path.join(process.cwd(), "src", "views"));
+app.set("views", config.paths.VIEWS);
 app.set("view engine", "ejs");
 
-// Especificar la ubicación de tus archivos de plantillas (views)
-app.set("views", config.paths.VIEWS);
-
-// Configurar archivos estáticos correctamente
+// Archivos estáticos (para imágenes, CSS, etc.)
 app.use(express.static(config.paths.PUBLIC));
 
+// Rutas
 app.use("/home", homeRouter);
 
-// --- RUTAS GENERALES DE LA APLICACIÓN ---
+// Redirige la raíz al home
 app.get("/", (req, res) => {
-  //res.json({url:config.SUPABASE_URL}) //prueba de que me conecto a
   res.redirect("/home");
 });
 

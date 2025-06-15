a14519502a3629cfb29eaed89f49a1af3cc565c7
PanaGomez <fabianfrancogomez@hotmail.com>
2025-06-15 20:55:42 -0300

    agregue los métodos que necesitamos para el ABM de productos a los routers controllers servicios y repository

--- server/src/routers/producto.router.js
+++ server/src/routers/producto.router.js
@@ -3,17 +3,30 @@ import { getProductosDestacadosController } from "../controllers/homepage.contro
 import {
   createProductoController,
   getAllProductosController,
+  getProductoByIdController,
+  updateProductoController,
+  deleteProductoController,
 } from "../controllers/producto.controller.js";
 
 const productoRouter = Router();
 
 // Get All - Productos
-productoRouter.get("/", getAllProductosController);
+productoRouter.get("/getAll", getAllProductosController);
 
 // Create One - Producto
-productoRouter.post("/", createProductoController);
+productoRouter.post("/createOne", createProductoController);
 
 // Get - ProductosDestacados
 productoRouter.get("/destacados", getProductosDestacadosController);
+
+// GET - Buscar un producto
+productoRouter.get("/:id", getProductoByIdController);
+
+// PUT - Actualizar un producto
+productoRouter.put("/:id", updateProductoController);
+
+// DELETE - Eliminar un producto
+productoRouter.delete("/:id", deleteProductoController);
+
 
 export default productoRouter;
